470061c1bdc0951cb47b36b0a5750a32ef05b3b7
ANDREW_FROST <andkab2014@yandex.com>
2020-07-27 19:33:50 +0700

    Update CompletePurchaseResponse.php

--- src/Message/CompletePurchaseResponse.php
+++ src/Message/CompletePurchaseResponse.php
@@ -39,7 +39,7 @@ class CompletePurchaseResponse extends AbstractResponse implements RedirectRespo
 
     public function getTransactionId()
     {
-        return intval($this->data['orderSum']);
+        return intval($this->data['unitpayId']);
     }
 
     public function getAmount()
